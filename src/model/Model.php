c8e6afd1e0179501a642c932505b7300e2bac253
Persi <persi.mankita97@gmail.com>
2021-05-01 14:20:46 +0200

    Ajout de l'UTF-8 pour l'affichage des caractères accentués

--- src/model/Model.php
+++ src/model/Model.php
@@ -6,7 +6,7 @@ class Model {
 
   static function connect()
   {
-    $dsn = "mysql:host=localhost;dbname=webstore";
+    $dsn = "mysql:host=localhost;dbname=webstore;charset=UTF8";
     $user = "root";
     $pass = "";
     return new \PDO($dsn, $user, $pass);
